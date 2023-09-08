b6db9516414bfbe3bdac24eead95629e909be6b1
ru51a4 <rruslanvaleev@yandex.ru>
2023-09-08 18:02:14 +0500

    api

--- src/components/items/item/item.jsx
+++ src/components/items/item/item.jsx
@@ -3,26 +3,26 @@ import style from './item.module.css';
 import { useDispatch } from "react-redux";
 import { setItemInCart } from "../../../redux/cart/cart-reducer";
 
-function Item(props){
+function Item(props) {
 
     const dispatch = useDispatch();
 
-    const handleClick = (e)=>{
+    const handleClick = (e) => {
         e.stopPropagation();
         dispatch(setItemInCart(props.item));
-        
+
     }
-    return(<div className={style.item}>
-        <img className={style.item__img} src={props.item.img}/>
-       <h1 className={style.item__h1}>{props.item.name}</h1>
-       <p className={style.item__country}>{props.item.country}</p>
-       <p className={style.item__price}>{props.item.price}</p>
-       <p className={style.item__material}>{props.item.material}</p>
-       <button 
-       type="primary"
-       onClick={handleClick}
-       className={style.item__button}>
-        Купить</button>
+    return (<div className={style.item}>
+        <img className={style.item__img} src={props.item.img} />
+        <h1 className={style.item__h1}>{props.item.name}</h1>
+        <p className={style.item__country}>{props.item.country}</p>
+        <p className={style.item__price}>{props.item.price}</p>
+        <p className={style.item__material}>{props.item.material}</p>
+        <button
+            type="primary"
+            onClick={handleClick}
+            className={style.item__button}>
+            Купить</button>
     </div>)
 }
 
